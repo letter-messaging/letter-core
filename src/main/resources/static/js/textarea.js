4f07ce7bb76c45e42dc1bc0dbba4d42cfb2bff47
vaniashows <vania6600@gmail.com>
2018-10-07 00:15:56 +0300

    frontend interface

---
+++ src/main/resources/static/js/textarea.js
@@ -0,0 +1,19 @@
+resize = (el) => {
+	let textArea = el;
+	textArea.style.height = '0px';
+	textArea.style.height = textArea.scrollHeight - 8 + 'px';
+};
+
+document.querySelector('#send-message-text').addEventListener('keydown', function (e) {
+		let textarea = document.getElementById("send-message-text");
+		if (e.keyCode === 13) {
+			if (e.ctrlKey) {
+				textarea.value += "\n";
+				resize(textarea);
+			}
+			else {
+				e.preventDefault();
+			}
+		}
+	}
+);
