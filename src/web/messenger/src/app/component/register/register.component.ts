d50b9f8a32ff4bab2e8dc62217bcf7f552fbcbd6
ivanjermakov <vania6600@gmail.com>
2019-01-02 04:28:41 +0300

    auth and register design

--- src/web/messenger/src/app/component/register/register.component.ts
+++ src/web/messenger/src/app/component/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss', './../auth/auth.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
