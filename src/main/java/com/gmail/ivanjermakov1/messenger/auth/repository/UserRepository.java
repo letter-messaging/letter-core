643d93552c4c8b46106036526231b160b1b660d7
ivanjermakov <vania6600@gmail.com>
2019-01-15 04:09:50 +0300

    api: cleanup

--- src/main/java/com/gmail/ivanjermakov1/messenger/auth/repository/UserRepository.java
+++ src/main/java/com/gmail/ivanjermakov1/messenger/auth/repository/UserRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
