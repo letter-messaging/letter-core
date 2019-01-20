9bb2b2f913a1d4e7164a33f80c9e8e40878f94f0
ivanjermakov <vania6600@gmail.com>
2019-01-20 15:51:42 +0300

    frontend: fix profile edit view after closing

--- src/web/messenger/src/app/component/messaging/outside-click.directive.ts
+++ src/web/messenger/src/app/component/messaging/outside-click.directive.ts
@@ -11,8 +11,8 @@ export class OutsideClickDirective {
   @Output('appOutsideClick') clickOutside: EventEmitter<any> = new EventEmitter();
 
   @HostListener('document:click', ['$event.target']) onMouseEnter(targetElement) {
-    const clickedInside = this._elementRef.nativeElement.contains(targetElement);
-    if (!clickedInside) {
+    if (targetElement.className === 'overlay') {
+      console.log('click outside!');
       this.clickOutside.emit(null);
     }
   }
