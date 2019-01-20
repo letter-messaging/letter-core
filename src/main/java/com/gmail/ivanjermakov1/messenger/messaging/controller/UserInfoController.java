f97e23e8d7826a9f823c1205b59aee4b429ee49a
ivanjermakov <vania6600@gmail.com>
2019-01-20 15:51:21 +0300

    api: cleanup

--- src/main/java/com/gmail/ivanjermakov1/messenger/messaging/controller/UserInfoController.java
+++ src/main/java/com/gmail/ivanjermakov1/messenger/messaging/controller/UserInfoController.java
@@ -3,7 +3,6 @@ package com.gmail.ivanjermakov1.messenger.messaging.controller;
 import com.gmail.ivanjermakov1.messenger.auth.entity.User;
 import com.gmail.ivanjermakov1.messenger.auth.service.UserService;
 import com.gmail.ivanjermakov1.messenger.exception.AuthenticationException;
-import com.gmail.ivanjermakov1.messenger.exception.NoSuchEntityException;
 import com.gmail.ivanjermakov1.messenger.messaging.dto.UserInfoDTO;
 import com.gmail.ivanjermakov1.messenger.messaging.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +29,7 @@ public class UserInfoController {
 	}
 	
 	@PostMapping
-	public UserInfoDTO edit(@RequestHeader("Auth-Token") String token, @RequestBody UserInfoDTO userInfoDTO) throws AuthenticationException, NoSuchEntityException {
+	public UserInfoDTO edit(@RequestHeader("Auth-Token") String token, @RequestBody UserInfoDTO userInfoDTO) throws AuthenticationException {
 		User user = userService.authenticate(token);
 		
 		return userInfoService.edit(user, userInfoDTO);
--- src/main/java/com/gmail/ivanjermakov1/messenger/messaging/service/UserInfoService.java
+++ src/main/java/com/gmail/ivanjermakov1/messenger/messaging/service/UserInfoService.java
@@ -2,7 +2,6 @@ package com.gmail.ivanjermakov1.messenger.messaging.service;
 
 import com.gmail.ivanjermakov1.messenger.auth.entity.User;
 import com.gmail.ivanjermakov1.messenger.exception.AuthenticationException;
-import com.gmail.ivanjermakov1.messenger.exception.NoSuchEntityException;
 import com.gmail.ivanjermakov1.messenger.messaging.dto.UserInfoDTO;
 import com.gmail.ivanjermakov1.messenger.messaging.entity.UserInfo;
 import com.gmail.ivanjermakov1.messenger.messaging.repository.UserInfoRepository;
@@ -35,7 +34,7 @@ public class UserInfoService {
 		return UserInfoDTO.map(userInfo);
 	}
 	
-	public UserInfoDTO edit(User user, UserInfoDTO userInfoDTO) throws AuthenticationException, NoSuchEntityException {
+	public UserInfoDTO edit(User user, UserInfoDTO userInfoDTO) throws AuthenticationException {
 		if (!userInfoDTO.getUser().getId().equals(user.getId()))
 			throw new AuthenticationException("allow only to edit personal info");
 		
