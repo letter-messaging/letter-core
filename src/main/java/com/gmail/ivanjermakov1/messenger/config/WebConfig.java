72ea21fb6f7862c978be61a97ec21cb04597f5cb
ivanjermakov <vania6600@gmail.com>
2019-03-11 23:38:11 +0300

    api: docs

--- src/main/java/com/gmail/ivanjermakov1/messenger/config/WebConfig.java
+++ src/main/java/com/gmail/ivanjermakov1/messenger/config/WebConfig.java
@@ -5,6 +5,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configure correct CORS policy for same host and different port REST API access
+ */
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
