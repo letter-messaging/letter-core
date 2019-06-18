e59c2a0a8e261e565f58ec592f11c07ef95fa075
ivanjermakov <vania6600@gmail.com>
2019-06-18 23:38:44 +0300

    frontend: re-login bugfix

--- web/messenger/src/app/component/embedded/menu/profile-menu/profile-menu.component.ts
+++ web/messenger/src/app/component/embedded/menu/profile-menu/profile-menu.component.ts
@@ -47,7 +47,7 @@ export class ProfileMenuComponent implements OnInit {
 
 	logout() {
 		this.cookieService.deleteToken();
-		this.router.navigate(['/auth']);
+		location.replace('/');
 	}
 
 	open(me: User) {
