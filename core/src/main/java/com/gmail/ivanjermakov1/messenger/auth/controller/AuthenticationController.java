d1584aaae63c6ca8a4634f5d40a1d587b8e61935
ivanjermakov <vania6600@gmail.com>
2019-06-18 11:55:05 +0300

    api: docs

--- core/src/main/java/com/gmail/ivanjermakov1/messenger/auth/controller/AuthenticationController.java
+++ core/src/main/java/com/gmail/ivanjermakov1/messenger/auth/controller/AuthenticationController.java
@@ -5,7 +5,11 @@ import com.gmail.ivanjermakov1.messenger.auth.entity.User;
 import com.gmail.ivanjermakov1.messenger.auth.service.UserService;
 import com.gmail.ivanjermakov1.messenger.exception.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth")
@@ -32,7 +36,7 @@ public class AuthenticationController {
 	}
 	
 	/**
-	 * Validate user token.
+	 * Validate user token. During validation user appears online.
 	 *
 	 * @param token user token
 	 * @return user
