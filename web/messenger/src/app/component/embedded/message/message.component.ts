6413f91bb8220abc70f377478a0cfc5878caab3d
ivanjermakov <vania6600@gmail.com>
2019-06-19 02:24:19 +0300

    frontend: style cleanup

--- web/messenger/src/app/component/embedded/message/message.component.ts
+++ web/messenger/src/app/component/embedded/message/message.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Message} from '../../../dto/Message';
 import {User} from '../../../dto/User';
-import {API_URL, FILE_URL} from '../../../../../globals';
+import {FILE_URL} from '../../../../../globals';
 import {ImageService} from '../../../service/image.service';
 import {ImageCompressionMode} from '../../../dto/enum/ImageCompressionMode';
 import {Document} from '../../../dto/Document';
@@ -47,6 +47,6 @@ export class MessageComponent implements OnInit {
 	}
 
 	openDocument(document: Document) {
-		window.open(FILE_URL + document.path, '_blank')
+		window.open(FILE_URL + document.path, '_blank');
 	}
 }
