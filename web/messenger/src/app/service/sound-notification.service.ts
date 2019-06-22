e06002933bb59eab018f69d27310cc4278ef1e8e
ivanjermakov <vania6600@gmail.com>
2019-06-22 22:47:06 +0300

    frontend: fix notifications

--- web/messenger/src/app/service/sound-notification.service.ts
+++ web/messenger/src/app/service/sound-notification.service.ts
@@ -8,10 +8,18 @@ export class SoundNotificationService {
 	private notificationAudio = new Audio('assets/sound/newmsg.mp3');
 
 	constructor() {
+		this.notificationAudio.load();
 	}
 
 	notify() {
-		this.notificationAudio.play();
+		let promise = this.notificationAudio.play();
+
+		// required due to browser policy
+		// described here: https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+		if (promise) {
+			promise.catch(e => {
+			});
+		}
 	}
 
 }
