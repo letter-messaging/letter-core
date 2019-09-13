78708abe5fae33aeb98046cfa1cfa75cedb44e28
ivanjermakov <vania6600@gmail.com>
2019-09-13 15:42:21 +0300

    api: add tests

--- core/src/test/java/com/gmail/ivanjermakov1/messenger/auth/UserTest.java
+++ core/src/test/java/com/gmail/ivanjermakov1/messenger/auth/UserTest.java
@@ -1,17 +1,25 @@
 package com.gmail.ivanjermakov1.messenger.auth;
 
+import com.gmail.ivanjermakov1.messenger.auth.service.UserService;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * TODO: test {@link com.gmail.ivanjermakov1.messenger.auth.service.UserService}'s scheduled {@code deleteOnline()}
- */
-@Ignore
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Transactional
 public class UserTest {
+
+	@Autowired
+	private UserService userService;
+
+	@Test
+	public void shouldDeleteOnlineRecords() {
+		userService.deleteOnline();
+	}
+
 }
