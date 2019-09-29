7941b77e18e31e36c4921e313e7888513ca384d5
ivanjermakov <vania6600@gmail.com>
2019-09-29 11:17:57 +0300

    api: validation improvements

--- core/src/main/java/com/gmail/ivanjermakov1/messenger/validator/MessageValidator.java
+++ core/src/main/java/com/gmail/ivanjermakov1/messenger/validator/MessageValidator.java
@@ -4,32 +4,27 @@ import com.github.ivanjermakov.jtrue.core.Validatable;
 import com.github.ivanjermakov.jtrue.core.Validator;
 import com.github.ivanjermakov.jtrue.predicate.NotEmptyCollection;
 import com.github.ivanjermakov.jtrue.predicate.NotNull;
-import com.gmail.ivanjermakov1.messenger.entity.Document;
-import com.gmail.ivanjermakov1.messenger.entity.Image;
 import com.gmail.ivanjermakov1.messenger.entity.Message;
 import com.gmail.ivanjermakov1.messenger.exception.InvalidEntityException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class MessageValidator implements Validatable<Message> {
 
-	private Validator<Message> attachmentValidator = new Validator<Message>()
-			.field(m -> m.forwarded, v -> v
-					.rule(new NotNull<List<Message>>()
-							.and(new NotEmptyCollection<>())
-							.negate()))
-			.field(m -> m.images, v -> v
-					.rule(new NotNull<List<Image>>()
-							.and(new NotEmptyCollection<>())
-							.negate()))
-			.field(m -> m.documents, v -> v
-					.rule(new NotNull<List<Document>>()
-							.and(new NotEmptyCollection<>())
-							.negate()));
+	private final Predicate<List<?>> notNullAndNotEmpty = new NotNull<List<?>>().and(new NotEmptyCollection<>());
 
-	private Validator<Message> validator = new Validator<Message>()
+	private final Validator<Message> attachmentValidator = new Validator<Message>()
+			.field(m -> m.forwarded, v -> v
+					.rule(notNullAndNotEmpty::test))
+			.field(m -> m.images, v -> v
+					.rule(notNullAndNotEmpty::test))
+			.field(m -> m.documents, v -> v
+					.rule(notNullAndNotEmpty::test));
+
+	private final Validator<Message> validator = new Validator<Message>()
 			.field(m -> m.sender, senderValidator -> senderValidator
 					.rule(new NotNull<>(), "user cannot be null")
 					.field(u -> u.id, senderIdValidator -> senderIdValidator
