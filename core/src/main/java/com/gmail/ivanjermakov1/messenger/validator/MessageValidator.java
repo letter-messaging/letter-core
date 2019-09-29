7420295b86975eb10efa8f13ab42c88013ad90fb
ivanjermakov <vania6600@gmail.com>
2019-09-29 16:41:34 +0300

    api: validation improvements

--- core/src/main/java/com/gmail/ivanjermakov1/messenger/validator/MessageValidator.java
+++ core/src/main/java/com/gmail/ivanjermakov1/messenger/validator/MessageValidator.java
@@ -4,6 +4,7 @@ import com.github.ivanjermakov.jtrue.core.Validatable;
 import com.github.ivanjermakov.jtrue.core.Validator;
 import com.github.ivanjermakov.jtrue.predicate.NotEmptyCollection;
 import com.github.ivanjermakov.jtrue.predicate.NotNull;
+import com.github.ivanjermakov.jtrue.predicate.Null;
 import com.gmail.ivanjermakov1.messenger.entity.Message;
 import com.gmail.ivanjermakov1.messenger.exception.InvalidEntityException;
 import org.springframework.stereotype.Component;
@@ -14,15 +15,15 @@ import java.util.function.Predicate;
 @Component
 public class MessageValidator implements Validatable<Message> {
 
-	private final Predicate<List<?>> notNullAndNotEmpty = new NotNull<List<?>>().and(new NotEmptyCollection<>());
+	private final Predicate<List<?>> nullOrEmpty = new Null<List<?>>().or(new NotEmptyCollection<>().negate());
 
 	private final Validator<Message> attachmentValidator = new Validator<Message>()
 			.field(m -> m.forwarded, v -> v
-					.rule(notNullAndNotEmpty::test))
+					.rule(nullOrEmpty::test))
 			.field(m -> m.images, v -> v
-					.rule(notNullAndNotEmpty::test))
+					.rule(nullOrEmpty::test))
 			.field(m -> m.documents, v -> v
-					.rule(notNullAndNotEmpty::test));
+					.rule(nullOrEmpty::test));
 
 	private final Validator<Message> validator = new Validator<Message>()
 			.field(m -> m.sender, senderValidator -> senderValidator
@@ -37,7 +38,7 @@ public class MessageValidator implements Validatable<Message> {
 							.rule(new NotNull<>(), "conversation id cannot be null")
 					)
 			)
-			.rule(m -> !m.text.isEmpty() || !attachmentValidator.validate(m), "message without text must contain attachments");
+			.rule(m -> !attachmentValidator.validate(m) || !(m == null || m.text.isEmpty()), "message without text must contain attachments");
 
 	@Override
 	public boolean validate(Message target) {
