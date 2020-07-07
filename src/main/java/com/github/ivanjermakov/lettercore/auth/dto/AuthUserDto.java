77b247dca6f85a7b74d5c1a776b144db26bc74f4
ivanjermakov <ivanjermakov1@gmail.com>
2020-07-07 13:48:47 +0300

    #18 Improve auth API

--- src/main/java/com/github/ivanjermakov/lettercore/auth/dto/AuthUserDto.java
+++ src/main/java/com/github/ivanjermakov/lettercore/auth/dto/AuthUserDto.java
@@ -5,6 +5,9 @@ public class AuthUserDto {
 	public String login;
 	public String password;
 
+	public AuthUserDto() {
+	}
+
 	public AuthUserDto(String login, String password) {
 		this.login = login;
 		this.password = password;
